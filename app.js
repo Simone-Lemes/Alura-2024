4e115116bb3293d6c753eab7048b59c5b41034c2
Simone-Lemes <68714091+Simone-Lemes@users.noreply.github.com>
2024-09-05 21:47:38 -0300

    aula 4
    finalização da aula 4

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
-const dadosCompletos = dadosAtletas.map(atleta => {
-    return {
-        esporte: atleta.esporte,
-        titulo: atleta.titulo,
-        foto: atleta.foto,
-        linkWikipedia: atleta.linkWikipedia,
-        instagram: atleta.instagram,
-        medalhas: atleta.medalhas
-    };
-})
 
-console.log(dadosAtletas);
+function pesquisar() {
+    const termoPesquisa = document.getElementById("campo-pesquisa").value.toLowerCase(); // Obtém o termo de pesquisa e converte para minúsculas
+    const section = document.getElementById("resultados-pesquisa");
+    let resultados = "";
+  
+    // Filtra os dados com base no termo de pesquisa
+    const resultadosFiltrados = dados.filter(dado => {
+      return dado.titulo.toLowerCase().includes(termoPesquisa) || 
+             dado.descricao.toLowerCase().includes(termoPesquisa);
+    });
+  
+    // Cria o HTML para os resultados filtrados
+    resultadosFiltrados.forEach(dado => {
+      resultados += `
+        <div class="item-resultado">
+          <h2>
+            <a href="${dado.link}" target="_blank">${dado.titulo}</a>
+          </h2>
+          <p class="descricao-meta">${dado.esporte}</p>
+          <p class="descricao-meta">${dado.medalhas}</p>
+          <img src="${dado.img}" alt="${dado.titulo}">
+          <p class="descricao-meta">${dado.descricao}</p>
+          <a href="${dado.link}" target="_blank">Mais informações</a>
+          <a href="${dado.instagram}" target="_blank">Instagram</a>
+          
+        </div>
+      `;
+    });
+  
+    section.innerHTML = resultados;
+}
--- dados.js
+++ dados.js
@@ -1,4 +1,4 @@
-const dadosAtletas = [
+/*let dadosAtletas = [
     {
         esporte: "Men's 100m - T37",
         titulo: "Ricardo Gomes de Mendonça",
@@ -191,5 +191,33 @@ const dadosAtletas = [
       instagram: "https://instagram.com/andreaelizaboscolo",
       medalhas: ["Medalha de Bronze"]
     }
+];*/
 
+
+
+   
+    
+
+
+
+let dados = [
+  {
+    titulo: "Michael Phelps",
+    descricao: "Michael Fred Phelps (1985) é um nadador profissional, conquistou trinta e sete recordes mundiais, incluindo o maior número de medalhas de ouro (oito) olímpicas em uma única edição.",
+    img: "img/michael.jpg",
+    link: "https://pt.wikipedia.org/wiki/Michael_Phelps"
+  },
+  
+  {
+    titulo: "Usain Bolt",
+    esporte: "Women's Club Throw - F32",
+    descricao: "Usain St. Leo Bolt (1986) é um ex-corredor jamaicano, considerado o homem mais rápido do mundo. É o recordista mundial nas provas de 100 metros e 200 metros rasos.",
+    img: "img/usain.jpg",
+    medalhas: "Medalha de Bronze",
+    link: "https://pt.wikipedia.org/wiki/Usain_Bolt",
+    instagram: "https://instagram.com/andreaelizaboscolo" 
+  },
 ];
+  
+
+
