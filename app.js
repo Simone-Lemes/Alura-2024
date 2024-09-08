a2c79f207ee1a3e94a68f3c8abaf9cffebb5f49e
Simone-Lemes <68714091+Simone-Lemes@users.noreply.github.com>
2024-09-07 21:53:12 -0300

    aula 5
    finalização da aula 5

--- app.js
+++ app.js
@@ -1,32 +1,37 @@
 
 function pesquisar() {
-    const termoPesquisa = document.getElementById("campo-pesquisa").value.toLowerCase(); // Obtém o termo de pesquisa e converte para minúsculas
-    const section = document.getElementById("resultados-pesquisa");
+    let campoPesquisa = document.getElementById("campo-pesquisa").value.toLowerCase(); // Obtém o termo de pesquisa e converte para minúsculas
+    let section = document.getElementById("resultados-pesquisa");
     let resultados = "";
   
-    // Filtra os dados com base no termo de pesquisa
-    const resultadosFiltrados = dados.filter(dado => {
-      return dado.titulo.toLowerCase().includes(termoPesquisa) || 
-             dado.descricao.toLowerCase().includes(termoPesquisa);
+    if (!campoPesquisa) {
+      section.innerHTML = "<p>Nenhum atleta foi encontrado</p>"
+      return
+    }
+  
+    // Filtra os dados com base no campo de pesquisa
+    let resultadosFiltrados = dados.filter(dado => {
+      return dado.titulo.toLowerCase().includes(campoPesquisa) ||     
+             dado.descricao.toLowerCase().includes(campoPesquisa); 
+             
     });
   
     // Cria o HTML para os resultados filtrados
     resultadosFiltrados.forEach(dado => {
       resultados += `
         <div class="item-resultado">
-          <h2>
-            <a href="${dado.link}" target="_blank">${dado.titulo}</a>
-          </h2>
-          <p class="descricao-meta">${dado.esporte}</p>
-          <p class="descricao-meta">${dado.medalhas}</p>
+          <h2><a href="${dado.link}" target="_blank">${dado.titulo}</a></h2>
           <img src="${dado.img}" alt="${dado.titulo}">
           <p class="descricao-meta">${dado.descricao}</p>
           <a href="${dado.link}" target="_blank">Mais informações</a>
-          <a href="${dado.instagram}" target="_blank">Instagram</a>
-          
         </div>
       `;
     });
+
+   if (!resultados) {
+       resultados = "<p> Nada foi encontrado </p>"
+
+   }
   
     section.innerHTML = resultados;
 }
